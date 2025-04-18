1bbd4a8d6cd3a3f5e8c86427b9244ad30fb13110
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-04-18 15:17:42 -0700

    reduce file size

--- ui/core_scripts/make_child.gd
+++ ui/core_scripts/make_child.gd
@@ -6,61 +6,17 @@ func make_child(dataDictionary, resolution, path, panel_name):
 	
 	var get_panel = preload("res://HevLib/ui/core_scripts/get_panel.gd")
 	
-	var d = {
-		"type":"panel_margin",
-		"texture":"panel_tl_br",
-		"topSpacePercent":20,
-		"leftSpacePercent":20,
-		"bottomSpacePercent":20,
-		"rightSpacePercent":20,
-		"square":false,
-		"vertical_align":"top",
-		"horizontal_align":"left",
-		"data":{}
-	}
-	for mir in dataDictionary:
-		match mir:
-			"type":
-				var dict = {"type":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"texture":
-				var dict = {"texture":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"rightSpacePercent":
-				var dict = {"rightSpacePercent":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"leftSpacePercent":
-				var dict = {"leftSpacePercent":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"topSpacePercent":
-				var dict = {"topSpacePercent":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"bottomSpacePercent":
-				var dict = {"bottomSpacePercent":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"square":
-				var dict = {"square":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"vertical_align":
-				var dict = {"vertical_align":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"horizontal_align":
-				var dict = {"horizontal_align":dataDictionary.get(mir)}
-				d.merge(dict, true)
-			"data":
-				var dict = {"data":dataDictionary.get(mir)}
-				d.merge(dict, true)
-#	var d = dataDictionary.get(data)
-	var type = d.get("type")
-	var texture = d.get("texture")
-	var rightSpacePercent = d.get("rightSpacePercent")
-	var leftSpacePercent = d.get("leftSpacePercent")
-	var topSpacePercent = d.get("topSpacePercent")
-	var bottomSpacePercent = d.get("bottomSpacePercent")
-	var square = d.get("square")
-	var vertical_align = d.get("vertical_align")
-	var horizontal_align = d.get("horizontal_align")
-	var paneldta = d.get("data")
+	var d = dataDictionary
+	var type = d.get("type", "panel_margin")
+	var texture = d.get("texture", "panel_tl_br")
+	var rightSpacePercent = d.get("rightSpacePercent", 20)
+	var leftSpacePercent = d.get("leftSpacePercent", 20)
+	var topSpacePercent = d.get("topSpacePercent", 20)
+	var bottomSpacePercent = d.get("bottomSpacePercent", 20)
+	var square = d.get("square", false)
+	var vertical_align = d.get("vertical_align", "top")
+	var horizontal_align = d.get("horizontal_align", "left")
+	var paneldta = d.get("data", {})
 	var checkdata = handle_resolution.handle_resolution(resolution,rightSpacePercent,leftSpacePercent,topSpacePercent,bottomSpacePercent,square,vertical_align, horizontal_align)
 	var paneldata = get_panel.get_panel(type,texture)
 
