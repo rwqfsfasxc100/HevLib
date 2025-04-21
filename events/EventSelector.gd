fa4347f39c9a9e5dced4f35f6bbda13cb07e1baf
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-04-20 21:39:03 -0700

    removed nonfunctional event prevention

--- events/EventSelector.gd
+++ events/EventSelector.gd
@@ -2,14 +2,15 @@ extends ScrollContainer
 
 onready var ring = get_node("/root/Game/TheRing")
 
-var all_events = []
+var all_events = {}
 
 func _ready():
 	
 	var events = ring.get_children()
 	for evnt in events:
 		var ename = evnt.name
-		all_events.append(ename)
+		var edict = {ename:true}
+		all_events.merge(edict)
 	var selector_button = load("res://HevLib/events/EventSelector.tscn")
 	for event in all_events:
 		var btn = selector_button.instance()
--- events/EventSelectorButton.gd
+++ events/EventSelectorButton.gd
@@ -2,24 +2,9 @@ extends CheckButton
 
 var event = ""
 
-var eventNode
-var ringNode
-
-func _ready():
-	for events in get_parent().get_parent().ring.get_children():
-		if events.name == event:
-			eventNode = events
-	ringNode = eventNode.get_parent()
-
-var NodeAccess = preload("res://HevLib/pointers/NodeAccess.gd").new()
+var isEnabled = true
 
 func _toggled(button_pressed):
-	if button_pressed:
-		var childs = self.get_child_count()
-		if childs == 1:
-			NodeAccess.__reparent(get_child(0),ringNode)
-
-	else:
-		var nt = ringNode.get_node(event)
-		NodeAccess.__reparent(nt, self)
-		
+	isEnabled = button_pressed
+	
+	get_parent().get_parent().all_events[event] = isEnabled
