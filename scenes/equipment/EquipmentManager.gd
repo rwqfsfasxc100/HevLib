5516bc4359ffd6599c1a9ab5514322832592ad31
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-04-28 00:19:20 -0700

    adjusted slot handling

--- scenes/equipment/EquipmentManager.gd
+++ scenes/equipment/EquipmentManager.gd
@@ -4,48 +4,41 @@ var itemSlot = preload("res://enceladus/SystemShipUpgradeUI.tscn")
 
 var hardpoint_types = [
 # Hardpoint slots
-"HARDPOINT", # - Any hardpoint
 "HARDPOINT_LOW_STRESS", # - Any low-stress hardpoint
 "HARDPOINT_HIGH_STRESS", # - Any high-stress hardpoint
 "HARDPOINT_SPINAL", # - Any rear hardpoint
 "HARDPOINT_DOCKING_BAY", # - A docking-bay type hardpoint
 "HARDPOINT_DRONE_POINT", # - A drone hardpoint
-
+]
+var alignments = [
 # Equipment alignment
 "ALIGNMENT_LEFT", # - Any left hardpoint
 "ALIGNMENT_RIGHT", # - Any right hardpoint
 "ALIGNMENT_CENTER", # - Any central hardpoint
+]
 
-# Hardpoint Capabilities
-"CAPABILITY_SIDE_ACCESS", # - Equipment has access to the side of the hardpoint
-"CAPABILITY_SHIP_REAR", # - Equipment can be placed in rear slots
-"CAPABILITY_HEAVY_EQUIPMENT", # - Equipment that is heavy and requires high-stress only. E.G. NANI and AR-1500
-"CAPABILITY_SHIP_SPINE", # - Equipment is heavy, but light enough to fit on any spinal mount
-"CAPABILITY_TURRETS", # - Can fit turreted equipment
-"CAPABILITY_CRADLES", # - Can fit cradles
-"CAPABILITY_NANODRONES", # - Can fit nanodrones
-"CAPABILITY_PLASMA_THROWER", # - Can fit plasma throwers
-"CAPABILITY_FRONT_FACING", # - Can fit front-facing equipment
+var equipment_types = [
+# Cradled equipment
+"EQUIPMENT_CARGO_CONTAINER",
+"EQUIPMENT_MINING_COMPANION",
+"EQUIPMENT_IMPACT_ABSORBER",
+"EQUIPMENT_BEACON",
 
-# Equipment capabilities
-"EQUIPMENT_HEAVY_DUTY",
-"EQUIPMENT_CARGO_CONTAINER", # - Any cargo container like equipment
-"EQUIPMENT_MINING_COMPANION", # - Any mining companion like equipment
-"EQUIPMENT_IMPACT_ABSORBER", # - Any impact absorber like equipment
-"EQUIPMENT_FRONT_FACING_WEAPON", # - Any front-facing equipment
-"EQUIPMENT_PLASMA_THROWER", # - Any plasma thrower equipment
-"EQUIPMENT_MANIPULATION_ARM", # - Any manipulator
-"EQUIPMENT_TURRET", # - Equipment is turreted
-"EQUIPMENT_NANODRONE_PLANT", # - Equipment uses nanodrones
-"EQUIPMENT_RAILGUN", # - Any railgun like equipment
-"EQUIPMENT_COILGUN", # - Any coilgun like equipment
-"EQUIPMENT_IRON_THROWER", # - Any iron thrower like equipment
-"EQUIPMENT_MINING_LASER", # - Any mining laser like equipment
-"EQUIPMENT_MICROWAVE", # - Any microwave emitter like equipment
-"EQUIPMENT_SYNCHROTRON", # - Any synchrotron like equipment
-"EQUIPMENT_BEACON", # - Any beacon like equipment
-
-# Other slot tags
+# Weapon tools
+"EQUIPMENT_MASS_DRIVERS",
+"EQUIPMENT_PLASMA_THROWERS",
+"EQUIPMENT_PLASMA_THROWERS_HEAVY",
+"EQUIPMENT_MANIPULATION_ARMS",
+"EQUIPMENT_TURRETS",
+"EQUIPMENT_NANODRONES",
+"EQUIPMENT_IRON_THROWERS",
+"EQUIPMENT_MINING_LASERS",
+"EQUIPMENT_MICROWAVES",
+"EQUIPMENT_SYNCHROTRONS",
+]
+var slot_types = [
+# Slot type tags
+"HARDPOINT",
 "MASS_DRIVER_AMMUNITION",
 "NANODRONE_STORAGE",
 "PROPELLANT_TANK",
@@ -62,82 +55,17 @@ var hardpoint_types = [
 "RECON_DRONE",
 ]
 
-var SUBTRACTIVES = []
 
-var HARDPOINT_CAPABILITIES = {
-	"HARDPOINT_LOW_STRESS":{
-		"alignment":"ALIGNMENT_CENTER",
-		"capabilities":[
-			"CAPABILITY_SIDE_ACCESS",
-			"CAPABILITY_TURRETS",
-			"CAPABILITY_CRADLES",
-			"CAPABILITY_NANODRONES",
-			"CAPABILITY_FRONT_FACING",
-		],
-		"equipment_overrides":[]
-	},
-	"HARDPOINT_HIGH_STRESS":{
-		"alignment":"ALIGNMENT_CENTER",
-		"capabilities":[
-			"CAPABILITY_HEAVY_EQUIPMENT",
-			"CAPABILITY_SHIP_SPINE",
-			"CAPABILITY_TURRETS",
-			"CAPABILITY_NANODRONES",
-			"CAPABILITY_PLASMA_THROWER",
-			"CAPABILITY_FRONT_FACING",
-		],
-		"equipment_overrides":[]
-	},
-	"HARDPOINT_SPINAL":{
-		"alignment":"ALIGNMENT_CENTER",
-		"capabilities":[
-			"CAPABILITY_SHIP_SPINE",
-			"CAPABILITY_TURRETS",
-			"CAPABILITY_NANODRONES",
-			"CAPABILITY_PLASMA_THROWER",
-			"CAPABILITY_FRONT_FACING",
-		],
-		"equipment_overrides":[]
-	},
-	"HARDPOINT_DOCKING_BAY":{
-		"alignment":"ALIGNMENT_CENTER",
-		"capabilities":[
-			"CAPABILITY_TURRETS",
-			"CAPABILITY_CRADLES",
-			"CAPABILITY_NANODRONES",
-		],
-		"equipment_overrides":["NOT_EQUIPMENT_BEACON"]
-	},
-	"HARDPOINT_DRONE_POINT":{
-		"alignment":"ALIGNMENT_CENTER",
-		"capabilities":[
-			"CAPABILITY_NANODRONES",
-		],
-		"equipment_overrides":[]
-	},
-}
-
-var EQUIPMENT_CAPABILITIES = {
-	"CAPABILITY_SIDE_ACCESS":["EQUIPMENT_CARGO_CONTAINER","EQUIPMENT_MINING_COMPANION","EQUIPMENT_IMPACT_ABSORBER","EQUIPMENT_BEACON"],
-	"CAPABILITY_SHIP_REAR":["EQUIPMENT_CARGO_CONTAINER","EQUIPMENT_MINING_COMPANION","EQUIPMENT_IMPACT_ABSORBER","EQUIPMENT_TURRET"],
-	"CAPABILITY_HEAVY_EQUIPMENT":["EQUIPMENT_HEAVY_DUTY"],
-	"CAPABILITY_SHIP_SPINE":["EQUIPMENT_FRONT_FACING_WEAPON","EQUIPMENT_PLASMA_THROWER"],
-	"CAPABILITY_TURRETS":["EQUIPMENT_TURRET"],
-	"CAPABILITY_CRADLES":["EQUIPMENT_CARGO_CONTAINER","EQUIPMENT_MINING_COMPANION","EQUIPMENT_IMPACT_ABSORBER","EQUIPMENT_BEACON"],
-	"CAPABILITY_NANODRONES":["EQUIPMENT_NANODRONE_PLANT"],
-	"CAPABILITY_PLASMA_THROWER":["EQUIPMENT_PLASMA_THROWER"],
-	"CAPABILITY_FRONT_FACING":["EQUIPMENT_FRONT_FACING_WEAPON"],
-}
 
 func _tree_entered():
 	get_tags()
-	make_subtractives()
 	add_slots()
 	add_slot_tags()
 	add_equipment()
 
+var slots = ModLoader.get_children()
+
 func add_slots():
-	var slots = ModLoader.get_children()
 	for slot in slots:
 		var data = slot.get_property_list()
 		var newSlot = null
@@ -179,13 +107,7 @@ func add_equipment():
 					groups = []
 				var current_slots = get_children()
 				for slt in current_slots:
-					var pdoes = check_groups(groups, slt.slotGroups)
-					if pdoes:
-						var slname = slt.name
-						if slname in eq_slots:
-							pass
-						else:
-							eq_slots.append(slname)
+					pass
 				for panel in eq_slots:
 					var parent = get_node(panel).get_node("VBoxContainer")
 					var itemTemplate = itemSlot.instance()
@@ -216,17 +138,6 @@ func add_equipment():
 						itemTemplate.name = equipment.get("name_override")
 					parent.add_child(itemTemplate)
 
-
-func check_groups(item_tags, compared_tags):
-	pass
-	
-	
-	
-	
-	
-	
-	
-
 func get_tags():
 	var slots = ModLoader.get_children()
 	for slot in slots:
@@ -237,17 +148,24 @@ func get_tags():
 				nodes = slot.get("EQUIPMENT_TAGS")
 		if not nodes == null:
 			for tag in nodes:
-				if tag in ADDITIVES:
-					pass
-				else:
-					ADDITIVES.append(tag)
-
-func make_subtractives():
-	for add in ADDITIVES:
-		SUBTRACTIVES.append("NOT_" + add)
+				var slotTypes = tag.get("slot_types",[])
+				var equipmentItems = tag.get("equipment_types",[])
+				var align = tag.get("alignments",[])
+				var hardpointTypes = tag.get("hardpoint_types",[])
+				if slotTypes.size() > 0:
+					for st in slotTypes:
+						slot_types.append(st)
+				if equipmentItems.size() > 0:
+					for st in equipmentItems:
+						equipment_types.append(st)
+				if align.size() > 0:
+					for st in align:
+						alignments.append(st)
+				if hardpointTypes.size() > 0:
+					for st in hardpointTypes:
+						hardpoint_types.append(st)
 
 func add_slot_tags():
-	var slots = ModLoader.get_children()
 	for slot in slots:
 		var data = slot.get_property_list()
 		var nodes = null
@@ -263,4 +181,4 @@ func add_slot_tags():
 							pass
 						else:
 							relevant_node.slotGroups.append(tag)
-		pass
+	pass
