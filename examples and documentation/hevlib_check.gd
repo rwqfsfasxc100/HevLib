2d334008e2016f88f9e4dcf02f81571ca9ff6ff5
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-08-12 12:39:36 -0700

    Update hevlib_check.gd

--- examples and documentation/hevlib_check.gd
+++ examples and documentation/hevlib_check.gd
@@ -8,7 +8,7 @@ extends Node
 # var mod_exists = mod_check.mod_exists
 
 
-# The mod name used by the dialogue box text when the mod does not exist
+# The display mod name used by the dialogue box text and for logging
 var mod_name : String = "HevLib"
 
 # The minimum and maximum versions of the mod that would be considered valid. 
@@ -16,6 +16,8 @@ var mod_name : String = "HevLib"
 # mod.manifest must be standardized to at least manifest version 2 to be treated as a valid version source
 # If no manifest exists, or the manifest version is either below 2 or is not stated, versioning in the ModMain.gd script requires the following int variables: MOD_VERSION_MAJOR, MOD_VERSION_MINOR, and MOD_VERSION_BUGFIX
 # If neither the manifest or mod main have proper versioning, the check automatically fails
+# Setting INF for the max major version and -INF for the min major version will act as standard operators and mean that no maximum or no minimum is set respectively
+# 
 var min_version_major : int = 1
 var min_version_minor : int = 0
 var min_version_bugfix : int = 0
@@ -24,14 +26,22 @@ var max_version_major : int = INF
 var max_version_minor : int = 6
 var max_version_bugfix : int = 9
 
+var check_mod_version : bool = true
 
-
+# Whether to display a confirmation dialogue box to say that the mod is missing
+# Will use a default message and mod name if the custom_message_string variable is left blank as ""
 var show_dialogue_box : bool = true
 
+# If true, the game will close after either the dialogue box is closed, or if show_dialogue_box is false, immediately after the query fails
+# If no dialogue box is used, there will be extra logging performed to make sure that the issue is made very clear.
 var crash_if_not_found : bool = true
 
+# The file path to the mod main file. The file structure is equivalent to the file structure of the zip file.
 var modmain_res_path : String = "res://HevLib/ModMain.gd"
 
+# A custom message that can be used for the dialogue box if enabled.
+# Can be both raw text or a translation string, however do make sure that the translation is loaded before this script runs
+# This will not use the mod_name string for display, so please make sure to include it in the string
 var custom_message_string : String = ""
 
 
@@ -40,12 +50,8 @@ var custom_message_string : String = ""
 
 
 
-
-
-
-
-
-var mod_exists : bool = false
+# Variable used to decide the query. Can be fetched if set to not close the game
+var mod_exists : bool
 
 
 
