0fc5365ac4ac305568e79d720cedc8615d697625
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-08-27 10:48:51 -0700

    requiring types for ManifestV2.1

--- scripts/manifest_v2/parse_file_as_manifest.gd
+++ scripts/manifest_v2/parse_file_as_manifest.gd
@@ -113,48 +113,48 @@ static func parse_file_as_manifest(file_path: String, format_to_manifest_version
 			2.1:
 				# information
 				if "mod_information" in manifest_data.keys():
-					dict_template["mod_information"]["id"] = manifest_data["mod_information"].get("id",null)
-					dict_template["mod_information"]["name"] = manifest_data["mod_information"].get("name",null)
-					dict_template["mod_information"]["description"] = manifest_data["mod_information"].get("description","HEVLIB_DESCRIPTION_PLACEHOLDER")
-					dict_template["mod_information"]["author"] = manifest_data["mod_information"].get("author","Unknown")
-					dict_template["mod_information"]["credits"] = manifest_data["mod_information"].get("credits",[])
+					dict_template["mod_information"]["id"] = String(manifest_data["mod_information"].get("id",null))
+					dict_template["mod_information"]["name"] = String(manifest_data["mod_information"].get("name",null))
+					dict_template["mod_information"]["description"] = String(manifest_data["mod_information"].get("description","HEVLIB_DESCRIPTION_PLACEHOLDER"))
+					dict_template["mod_information"]["author"] = String(manifest_data["mod_information"].get("author","Unknown"))
+					dict_template["mod_information"]["credits"] = PoolStringArray(manifest_data["mod_information"].get("credits",[]))
 				
 				# versioning
 				if "version" in manifest_data.keys():
-					dict_template["version"]["version_major"] = manifest_data["version"].get("version_major",1)
-					dict_template["version"]["version_minor"] = manifest_data["version"].get("version_minor",0)
-					dict_template["version"]["version_bugfix"] = manifest_data["version"].get("version_bugfix",0)
-					dict_template["version"]["version_metadata"] = manifest_data["version"].get("version_metadata","")
+					dict_template["version"]["version_major"] = int(manifest_data["version"].get("version_major",1))
+					dict_template["version"]["version_minor"] = int(manifest_data["version"].get("version_minor",0))
+					dict_template["version"]["version_bugfix"] = int(manifest_data["version"].get("version_bugfix",0))
+					dict_template["version"]["version_metadata"] = String(manifest_data["version"].get("version_metadata",""))
 				
 				# tags
 				if "tags" in manifest_data.keys():
-					dict_template["tags"]["allow_achievements"] = manifest_data["tags"].get("allow_achievements",false)
-					dict_template["tags"]["adds_ships"] = manifest_data["tags"].get("adds_ships",[])
-					dict_template["tags"]["adds_equipment"] = manifest_data["tags"].get("adds_equipment",[])
-					dict_template["tags"]["quality_of_life"] = manifest_data["tags"].get("quality_of_life",false)
-					dict_template["tags"]["is_library_mod"] = manifest_data["tags"].get("is_library_mod",false)
-					dict_template["tags"]["adds_gameplay_mechanics"] = manifest_data["tags"].get("adds_gameplay_mechanics",[])
-					dict_template["tags"]["uses_hevlib_research"] = manifest_data["tags"].get("uses_hevlib_research",false)
-					dict_template["tags"]["overhaul"] = manifest_data["tags"].get("overhaul",false)
-					dict_template["tags"]["adds_events"] = manifest_data["tags"].get("adds_events",[])
-					dict_template["tags"]["handle_extra_crew"] = manifest_data["tags"].get("handle_extra_crew",24)
-					dict_template["tags"]["visual"] = manifest_data["tags"].get("visual",false)
-					dict_template["tags"]["fun"] = manifest_data["tags"].get("fun",false)
-					dict_template["tags"]["language"] = manifest_data["tags"].get("language",["en"])
+					dict_template["tags"]["allow_achievements"] = bool(manifest_data["tags"].get("allow_achievements",false))
+					dict_template["tags"]["adds_ships"] = PoolStringArray(manifest_data["tags"].get("adds_ships",[]))
+					dict_template["tags"]["adds_equipment"] = PoolStringArray(manifest_data["tags"].get("adds_equipment",[]))
+					dict_template["tags"]["quality_of_life"] = bool(manifest_data["tags"].get("quality_of_life",false))
+					dict_template["tags"]["is_library_mod"] = bool(manifest_data["tags"].get("is_library_mod",false))
+					dict_template["tags"]["adds_gameplay_mechanics"] = PoolStringArray(manifest_data["tags"].get("adds_gameplay_mechanics",[]))
+					dict_template["tags"]["uses_hevlib_research"] = bool(manifest_data["tags"].get("uses_hevlib_research",false))
+					dict_template["tags"]["overhaul"] = bool(manifest_data["tags"].get("overhaul",false))
+					dict_template["tags"]["adds_events"] = PoolStringArray(manifest_data["tags"].get("adds_events",[]))
+					dict_template["tags"]["handle_extra_crew"] = float(manifest_data["tags"].get("handle_extra_crew",24))
+					dict_template["tags"]["visual"] = bool(manifest_data["tags"].get("visual",false))
+					dict_template["tags"]["fun"] = bool(manifest_data["tags"].get("fun",false))
+					dict_template["tags"]["language"] = PoolStringArray(manifest_data["tags"].get("language",["en"]))
 				
 				# links
 				if "links" in manifest_data.keys():
-					dict_template["links"]["github"] = manifest_data["links"].get("github",{"link":"","has_releases":false})
-					dict_template["links"]["discord"] = manifest_data["links"].get("discord","")
-					dict_template["links"]["nexus"] = manifest_data["links"].get("nexus","")
-					dict_template["links"]["donations"] = manifest_data["links"].get("donations","")
-					dict_template["links"]["wiki"] = manifest_data["links"].get("wiki","")
-					dict_template["links"]["bug_reports"] = manifest_data["links"].get("bug_reports","")
-					dict_template["links"]["custom_links"] = manifest_data["links"].get("custom_links",{})
+					dict_template["links"]["github"] = Dictionary(manifest_data["links"].get("github",{"link":"","has_releases":false}))
+					dict_template["links"]["discord"] = String(manifest_data["links"].get("discord",""))
+					dict_template["links"]["nexus"] = String(manifest_data["links"].get("nexus",""))
+					dict_template["links"]["donations"] = String(manifest_data["links"].get("donations",""))
+					dict_template["links"]["wiki"] = String(manifest_data["links"].get("wiki",""))
+					dict_template["links"]["bug_reports"] = String(manifest_data["links"].get("bug_reports",""))
+					dict_template["links"]["custom_links"] = Dictionary(manifest_data["links"].get("custom_links",{}))
 				
 				# manifest definitions
 				if "manifest_definitions" in manifest_data.keys():
-					dict_template["manifest_definitions"]["manifest_version"] = manifest_data["manifest_definitions"].get("manifest_version",manifest_version)
+					dict_template["manifest_definitions"]["manifest_version"] = float(manifest_data["manifest_definitions"].get("manifest_version",manifest_version))
 					dict_template["manifest_definitions"]["dependancy_mod_ids"] = PoolStringArray(manifest_data["manifest_definitions"].get("dependancy_mod_ids",[]))
 					dict_template["manifest_definitions"]["conflicting_mod_ids"] = PoolStringArray(manifest_data["manifest_definitions"].get("conflicting_mod_ids",[]))
 					dict_template["manifest_definitions"]["complimentary_mod_ids"] = PoolStringArray(manifest_data["manifest_definitions"].get("complimentary_mod_ids",[]))
