5dfb6cbb268a1961185d6ae84772ed62e8f67bab
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-09-04 21:30:16 -0700

    removed test code

--- scenes/scene_replacements/ship-ctrl.gd
+++ scenes/scene_replacements/ship-ctrl.gd
@@ -5,22 +5,22 @@ var HUDs = []
 
 func _ready():
 	var mname = self.shipName
-	if !self.cutscene and self.isPlayerControlled():
-		var children = get_children()
-		for child in children:
-			var scriptobj = child.get_script()
-			if scriptobj != null:
-				var script = scriptobj.get_path()
-				if script == "res://hud/Hud.gd":
-					child.set_script(scriptobj)
-				if script == "res://ships/modules/MineralProcessingUnit.gd":
-					child.set_script(scriptobj)
-		breakpoint
+#	if !self.cutscene and self.isPlayerControlled():
+#		var children = get_children()
+#		for child in children:
+#			var scriptobj = child.get_script()
+#			if scriptobj != null:
+#				var script = scriptobj.get_path()
+#				if script == "res://hud/Hud.gd":
+#					child.set_script(scriptobj)
+#				if script == "res://ships/modules/MineralProcessingUnit.gd":
+#					child.set_script(scriptobj)
+#	breakpoint
 
 #func _process(delta):
 #	if !self.cutscene and self.isPlayerControlled():
 #		breakpoint
 
-func _input(event):
-	if !self.cutscene and self.isPlayerControlled():
-		breakpoint
+#func _input(event):
+#	if !self.cutscene and self.isPlayerControlled():
+#		breakpoint
