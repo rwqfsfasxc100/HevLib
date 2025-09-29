6bbdd1fef67d58991c58e84ea7ebe8eb60773144
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-09-28 19:55:55 -0700

    added __get_folder_structure

---
+++ scripts/get_folder_structure.gd
@@ -0,0 +1,20 @@
+extends Node
+
+static func get_folder_structure(folder,store_file_content = false):
+	var FolderAccess = preload("res://HevLib/pointers/FolderAccess.gd")
+	var file = File.new()
+	var folder_structure = {}
+	var files = FolderAccess.__fetch_folder_files(folder,true,false)
+	for object in files:
+		if object.ends_with("/"):
+			var data = FolderAccess.__get_folder_structure(folder+object,store_file_content)
+			folder_structure.merge({object:data})
+		else:
+			var fd = "FILE"
+			if store_file_content:
+				file.open(folder + object,File.READ)
+				fd = file.get_as_text(true)
+				file.close()
+			folder_structure.merge({object:fd})
+	return folder_structure
+	
