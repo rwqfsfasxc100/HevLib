401ed3b7327b0ee9eaa284cee66097103bdba279
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-10-16 20:25:03 -0700

    updated research

--- scenes/research/Research.gd
+++ scenes/research/Research.gd
@@ -5,6 +5,9 @@ var lastFocus
 export var research_button_path = NodePath("")
 onready var research_button = get_node(research_button_path)
 
+onready var current_project_management = $TabHintContainer/Tabs/HEVLIB_RESEARCH_CURRENT/CurrentResearchManagement
+onready var dormant_project_management = $TabHintContainer/Tabs/HEVLIB_RESEARCH_AVAILABLE/MarginContainer/HBoxContainer/ActivatableProjects
+
 func show():
 	if visible or $Shower.is_playing():
 		return
@@ -23,6 +26,13 @@ func _ready():
 	var tag_exists = ManifestV2.__get_tags()
 	if not "TAG_USING_HEVLIB_RESEARCH" in tag_exists:
 		Tool.remove(research_button)
+	else:
+		var state = CurrentGame.state
+		if not "hevlib_research" in state:
+			CurrentGame.state.merge({"hevlib_research":{}})
+		get_research_data()
+
+var research_state = {}
 
 const ManifestV2 = preload("res://HevLib/pointers/ManifestV2.gd")
 
@@ -40,3 +50,20 @@ func unfocus():
 
 func _on_Research_pressed():
 	show()
+
+func get_research_data():
+	research_state = CurrentGame.state.hevlib_research
+	var tags = ManifestV2.__get_mods_and_tags_from_tag("TAG_USING_HEVLIB_RESEARCH")
+	for mod in tags:
+		for p in tags[mod]:
+			var id = mod + "_" + p.name
+			p.merge({"source":mod})
+			if not id in research_state:
+				var state = {
+					"active":false,
+					"time_while_active":-1,
+				}
+				p.merge({"state":state})
+			research_state.merge({id:p},true)
+	current_project_management._initialize()
+	dormant_project_management._initialize()
---
+++ scenes/research/research_item_box/ResearchItem.gd
@@ -0,0 +1,15 @@
+extends HBoxContainer
+
+export var this_research_project = {}
+
+onready var sub_progress_container = $Progress/SubBarProgress/HBoxContainer
+onready var total_progress_container = $Progress/FullProgress
+
+func _ready():
+	pass
+
+
+
+
+
+var default_vp_positioning = {"position":Vector2(170,170),"rotation":90,"scale":Vector2(1,1)}
