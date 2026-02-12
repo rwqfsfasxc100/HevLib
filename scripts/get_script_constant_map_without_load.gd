5ea3d01eff57e882a7dfcb9bbc280a7312604db3
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-02-12 01:58:09 -0800

    fixed and refactored `__get_script_constant_map_without_load`

--- scripts/get_script_constant_map_without_load.gd
+++ scripts/get_script_constant_map_without_load.gd
@@ -1,60 +1,20 @@
 extends Node
 
-static func get_script_constant_map_without_load(script_path : String):
+static func get_script_constant_map_without_load(script_path,trim_scripts) -> Dictionary:
+	var filepath = "user://cache/.HevLib_Cache/"
+	var pathway = trim_scripts.trim_scripts(script_path)
+	if pathway[2].size() == 0:
+		return {}
 	var file = File.new()
-	if file.open(script_path,File.READ):
-		return []
-	else:
-		var data = file.get_as_text(true)
-		file.close()
-		var constants = {}
-		var cname = ""
-		var cval = ""
-		for line in data.split("\n"):
-			if line.to_lower().begins_with("const "):
-				if cname != "":
-					constants[str2var(cname)] = str2var(str(cval))
-				cname = ""
-				cval = ""
-				line.erase(0,6)
-				var l = line.split("=")
-				var n = l[0]
-				while n.begins_with(" "):
-					n = n.lstrip(" ")
-				while n.ends_with(" "):
-					n = n.rstrip(" ")
-				var val = l[1]
-				if typeof(val) == TYPE_STRING:
-					while val.begins_with(" "):
-						val = val.lstrip(" ")
-					while val.ends_with(" "):
-						val = val.rstrip(" ")
-					if val == "INF":
-						val = INF
-					elif val == "-INF":
-						val = -INF
-					elif val == "PI":
-						val = PI
-					elif val == "-PI":
-						val = -PI
-					elif val == "TAU":
-						val = TAU
-					elif val == "-TAU":
-						val = -TAU
-					elif val == "NAN":
-						val = NAN
-					elif val == "-NAN":
-						val = -NAN
-				cname = n
-				cval = val
-				
-			else:
-				var vp = line.strip_escapes().length()
-				if vp:
-					cval = cval + "\n" + line.split("#")[0]
-				else:
-					if cname != "":
-						constants[str2var(cname)] = str2var(str(cval))
-					cname = ""
-					cval = ""
-		return constants
+	var dir = Directory.new()
+	var n = filepath + str(Time.get_ticks_usec()) + ".gd"
+	file.open(n,File.WRITE)
+	file.store_string(pathway[0])
+	file.close()
+	var dict = {}
+	var l = load(n).new().get_script().get_script_constant_map()
+	for i in pathway[2]:
+		var fd = l.get(i)
+		dict.merge({i:fd})
+	dir.remove(n)
+	return dict
