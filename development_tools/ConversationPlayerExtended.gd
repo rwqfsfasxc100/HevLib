a1295f06c95403e484a3abd55ed7852b3df84353
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-03 11:11:39 -0700

    moved comms extender script to the development_tools folder and added comments

--- development_tools/ConversationPlayerExtended.gd
+++ development_tools/ConversationPlayerExtended.gd
@@ -1,7 +1,12 @@
+# This script should be used for modified comms nodes and adds extra functionality.
 extends "res://comms/ConversationPlayer.gd"
 
+# Used to spawn an event when the conversation is run
 export (String) var spawnEvent = ""
 
+# Used to check against a config for any succeeding option
+# Config must be valid (i.e. not returns null when checked), 
+# and all three entries must be filled out to be used
 export (String) var config_ID = ""
 export (String) var config_section = ""
 export (String) var config_setting = ""
