b0999de86c6c30209222d98528739511f965023b
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-07 18:49:22 -0700

    added select and deselect all buttons to events menu

--- events/EventSelector.gd
+++ events/EventSelector.gd
@@ -6,8 +6,13 @@ var all_events = {}
 #const ConfigDriver = preload("res://HevLib/pointers/ConfigDriver.gd")
 #func _process(delta):
 #	get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().selected_events = all_events
-
+onready var list = $VBoxContainer
+onready var enableAllButton = get_node_or_null(NodePath("../HBoxContainer/EnableAll"))
+onready var disableAllButton = get_node_or_null(NodePath("../HBoxContainer/DisableAll"))
 func _ready():
+	enableAllButton.connect("pressed",self,"enableAll")
+	disableAllButton.connect("pressed",self,"disableAll")
+	
 #	var de = ConfigDriver.__get_value("HevLib","HEVLIB_CONFIG_SECTION_EVENTS","disabled_events")
 #	if de == null:
 #		
@@ -33,4 +38,15 @@ func _ready():
 		btn.text = event
 		btn.name = event
 		btn.event = event
-		$VBoxContainer.add_child(btn)
+		list.add_child(btn)
+
+func enableAll():
+	for c in list.get_children():
+		c.pressed = true
+	pointers.ConfigDriver.__store_value("HevLib","HEVLIB_CONFIG_SECTION_EVENTS","disabled_events",[])
+func disableAll():
+	var arr = []
+	for c in list.get_children():
+		c.pressed = false
+		arr.append(c.event)
+	pointers.ConfigDriver.__store_value("HevLib","HEVLIB_CONFIG_SECTION_EVENTS","disabled_events",arr)
