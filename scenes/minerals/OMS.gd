2f827f1f6db0280f40bd28f7c74e14a447c037bb
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-10 16:48:27 -0700

    put geologist tab info displays side-by-side

--- scenes/minerals/OMS.gd
+++ scenes/minerals/OMS.gd
@@ -18,6 +18,29 @@ func _ready():
 	geo.remove_child(mineral)
 	mineral.size_flags_vertical = SIZE_FILL
 	scroll.add_child(mineral)
+	
+	var cargo = geo.get_node(NodePath("MarginContainer/Cargo Panel"))
+	var vb2 = cargo.get_node(NodePath("VBoxContainer2"))
+	var rrect = vb2.rect_size
+	var hb = HBoxContainer.new()
+	cargo.remove_child(vb2)
+	cargo.add_child(hb)
+	cargo.move_child(hb,0)
+	var vb = VBoxContainer.new()
+	var l1 = vb2.get_node("Label List")
+	var l2 = vb2.get_node("CargoManifest")
+	vb2.remove_child(l1)
+	vb2.remove_child(l2)
+	vb.add_child(l1)
+	vb.add_child(l2)
+	hb.add_child(vb)
+	hb.rect_size = rrect
+	hb.size_flags_horizontal = SIZE_EXPAND_FILL
+	var splitter = VBoxContainer.new()
+	splitter.rect_min_size = Vector2(5,0)
+	hb.add_child(splitter)
+	hb.add_child(vb2)
+	
 	operated = true
 func _process(delta):
 	if operated:
