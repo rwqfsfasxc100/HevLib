1b4c0b389a46303389b1cfe83659094582924621
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-11 02:47:20 -0700

    started work for event handler parameters

--- events/event_handler.gd
+++ events/event_handler.gd
@@ -12,7 +12,9 @@ func _ready():
 	timer.name = "Timer"
 	add_child(timer)
 var focusObject
-func spawn_event(event,thering: Node):
+
+const default_parameter_dict = {}
+func spawn_event(event,thering: Node,parameters : Dictionary = default_parameter_dict.duplicate(true)):
 	if thering == null:
 		Debug.l("No ring object specified, returning")
 		return
@@ -22,7 +24,7 @@ func spawn_event(event,thering: Node):
 		var tree = ring.get_tree()
 		var event_node = ring.get_node_or_null(event)
 		if event_node and event_node.has_method("canBeAt") and event_node.has_method("makeAt"):
-			var pos = getPos()
+			var pos = getPos(parameters)
 			event_node.canBeAt(pos)
 			var oddity = event_node.makeAt(pos)
 			if oddity and ring.has_method("request_event"):
@@ -37,21 +39,26 @@ func spawn_event(event,thering: Node):
 #		startEventTimerNode()
 
 
-var spawnDirectionScale = 0.75
-var odditySpawnRadiusMin = 24000
-var odditySpawnRadiusMinCutscene = 6000
-var odditySpawnRadiusMax = 38000
-var odditySpawnRadiusSafemax = 80000
-var odditySpawnFailures = 0
-var odditySpawnRadiusSafemaxSteps = 40
 
-func getPos():
+
+func getPos(params : Dictionary):
+	
+	var x_direction = params.get("x_direction",rand_range( - 1, 1))
+	var y_direction = params.get("y_direction",rand_range( - 1, 1))
+	
+	var spawnDirectionScale = params.get("spawn_direction_scale",0.75)
+	var odditySpawnRadiusMin = params.get("oddity_spawn_radius_min",24000)
+	var odditySpawnRadiusMinCutscene = params.get("oddity_spawn_radius_min_cutscene",6000)
+	var odditySpawnRadiusMax = params.get("oddity_spawn_radius_max",38000)
+	var odditySpawnRadiusSafemax = params.get("oddity_spawn_radius_safe_max",80000)
+	var odditySpawnFailures = params.get("oddity_spawn_failures",0)
+	var odditySpawnRadiusSafemaxSteps = params.get("oddity_spawn_radius_safe_max_steps",40)
 	
 	if Tool.claim(focusObject):
 		
 		var cutscene = ("cutscene" in focusObject and focusObject.cutscene) and ("fastTravelDirection" in focusObject and focusObject.fastTravelDirection < 0)
 		var focusPoint = focusObject.global_position
-		var randomVector = Vector2(rand_range( - 1, 1), rand_range( - 1, 1)).normalized()
+		var randomVector = Vector2(clamp(x_direction,-1,1),clamp(y_direction,-1,1)).normalized()
 		var directionVector = focusObject.linear_velocity.normalized() * spawnDirectionScale
 		var dirvec = (randomVector + directionVector).normalized()
 		if dirvec.length() < 0.9:
