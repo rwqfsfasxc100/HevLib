6fae0940966a1127c10bb74e6af8186ec69cfe70
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-21 17:17:08 -0700

    made processed cargo hider object use a unique variable

--- scenes/minerals/ProcessedCargoManifest.gd
+++ scenes/minerals/ProcessedCargoManifest.gd
@@ -1,10 +1,10 @@
 extends "res://hud/trtl/ProcessedCargoManifest.gd"
 
-var obj
+var processed_cargo_limiter_obj
 
 func _ready():
 	var p = CurrentGame.get_tree().get_root().get_node_or_null("HevLib~Pointers")
-	obj = load("res://HevLib/scenes/minerals/ShipInterrupt.gd").new(p)
-	obj.ship = ship
-	ship = obj
+	processed_cargo_limiter_obj = load("res://HevLib/scenes/minerals/ShipInterrupt.gd").new(p)
+	processed_cargo_limiter_obj.ship = ship
+	ship = processed_cargo_limiter_obj
 
