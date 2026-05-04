46b5a6d27fc46df3e2d6e34423c2a0c7e5c3dc58
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-05-03 21:38:16 -0700

    added require_restart tag to bool, int, float, option button, and string config inputs

--- ui/mod_menu/settings_menus/entry_inputs/string.gd
+++ ui/mod_menu/settings_menus/entry_inputs/string.gd
@@ -8,7 +8,8 @@ var CONFIG_SECTION = ""
 
 var CONFIG_MOD = ""
 onready var pointers = get_tree().get_root().get_node_or_null("HevLib~Pointers")
-#const ConfigDriver = preload("res://HevLib/pointers/ConfigDriver.gd")
+
+var volatile = false
 
 func _ready():
 	var value = pointers.ConfigDriver.__get_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY)
@@ -21,95 +22,96 @@ func _ready():
 	$LineEdit.clear_button_enabled = CONFIG_DATA.get("clear_button",false)
 	$LineEdit.placeholder_text = CONFIG_DATA.get("placeholder","HEVLIB_CONFIG_LINEEDIT_PLACEHOLDER")
 	$Label/LABELBUTTON.hint_tooltip = CONFIG_DATA.get("description","")
+	volatile = CONFIG_DATA.get("require_restart",false)
 	add_to_group("hevlib_settings_tab",true)
 
 
 func recheck_availability():
-#	if not $LineEdit.has_focus():
-		
-		$LineEdit.text = pointers.ConfigDriver.__get_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY)
-		if $LineEdit.text != CONFIG_DATA.get("default",""):
-			$reset.visible = true
-			$Label/LABELBUTTON.focus_neighbour_right = $Label/LABELBUTTON.get_path_to($reset)
-		else:
-			$reset.visible = false
-			$Label/LABELBUTTON.focus_neighbour_right = $Label/LABELBUTTON.get_path_to($LineEdit)
-		var requirements = PoolStringArray(CONFIG_DATA.get("requires_bools",[]))
-		if requirements.size() >= 1:
-			var show = true
-			var valid_options = 0
-			var true_valids = 0
-			var flip = CONFIG_DATA.get("invert_bool_requirement",false)
-			for option in requirements:
-				
-				var split = option.split("/")
-				if split.size() == 3:
-					var value = pointers.ConfigDriver.__get_value(split[0],split[1],split[2])
-					if typeof(value) == TYPE_BOOL:
-						valid_options += 1
-						if value == true:
-							true_valids += 1
-			if valid_options >= 1:
-				if flip:
-					if true_valids >= 1:
-						$reset.modulate = Color(0.6,0.6,0.6,1)
-						$reset.disabled = true
-						$LineEdit.modulate = Color(0.6,0.6,0.6,1)
-						$LineEdit.editable = false
-					else:
-						$reset.modulate = Color(1,1,1,1)
-						$reset.disabled = false
-						$LineEdit.modulate = Color(1,1,1,1)
-						$LineEdit.editable = true
+	$LineEdit.text = pointers.ConfigDriver.__get_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY)
+	if $LineEdit.text != CONFIG_DATA.get("default",""):
+		$reset.visible = true
+		$Label/LABELBUTTON.focus_neighbour_right = $Label/LABELBUTTON.get_path_to($reset)
+	else:
+		$reset.visible = false
+		$Label/LABELBUTTON.focus_neighbour_right = $Label/LABELBUTTON.get_path_to($LineEdit)
+	var requirements = PoolStringArray(CONFIG_DATA.get("requires_bools",[]))
+	if requirements.size() >= 1:
+		var show = true
+		var valid_options = 0
+		var true_valids = 0
+		var flip = CONFIG_DATA.get("invert_bool_requirement",false)
+		for option in requirements:
+			
+			var split = option.split("/")
+			if split.size() == 3:
+				var value = pointers.ConfigDriver.__get_value(split[0],split[1],split[2])
+				if typeof(value) == TYPE_BOOL:
+					valid_options += 1
+					if value == true:
+						true_valids += 1
+		if valid_options >= 1:
+			if flip:
+				if true_valids >= 1:
+					$reset.modulate = Color(0.6,0.6,0.6,1)
+					$reset.disabled = true
+					$LineEdit.modulate = Color(0.6,0.6,0.6,1)
+					$LineEdit.editable = false
 				else:
-					if true_valids >= 1:
-						$reset.modulate = Color(1,1,1,1)
-						$reset.disabled = false
-						$LineEdit.modulate = Color(1,1,1,1)
-						$LineEdit.editable = true
-					else:
-						$reset.modulate = Color(0.6,0.6,0.6,1)
-						$reset.disabled = true
-						$LineEdit.modulate = Color(0.6,0.6,0.6,1)
-						$LineEdit.editable = false
-		else:
-			$reset.modulate = Color(1,1,1,1)
-			$reset.disabled = false
-			$LineEdit.modulate = Color(1,1,1,1)
-			$LineEdit.editable = true
-#	$LineEdit.caret_position = caret_pos
+					$reset.modulate = Color(1,1,1,1)
+					$reset.disabled = false
+					$LineEdit.modulate = Color(1,1,1,1)
+					$LineEdit.editable = true
+			else:
+				if true_valids >= 1:
+					$reset.modulate = Color(1,1,1,1)
+					$reset.disabled = false
+					$LineEdit.modulate = Color(1,1,1,1)
+					$LineEdit.editable = true
+				else:
+					$reset.modulate = Color(0.6,0.6,0.6,1)
+					$reset.disabled = true
+					$LineEdit.modulate = Color(0.6,0.6,0.6,1)
+					$LineEdit.editable = false
+	else:
+		$reset.modulate = Color(1,1,1,1)
+		$reset.disabled = false
+		$LineEdit.modulate = Color(1,1,1,1)
+		$LineEdit.editable = true
 
 
 func _reset_pressed():
-	$LineEdit.text = CONFIG_DATA.get("default","")
-	pointers.ConfigDriver.__store_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY,CONFIG_DATA.get("default",""))
+	var defaultVal = CONFIG_DATA.get("default","")
+	$LineEdit.text = defaultVal
+	if volatile:
+		var old_val = pointers.ConfigDriver.__get_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY)
+		if old_val != defaultVal:
+			triggerVolatile()
+	pointers.ConfigDriver.__store_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY,defaultVal)
 	$LineEdit.grab_focus()
 	$reset.visible = false
-#	$LineEdit.caret_position = caret_pos
 func _draw():
-
 	refocus()
 
 func refocus():
 	$Label/LABELBUTTON.rect_size = $Label.rect_size
-#	get_tree().call_group("hevlib_settings_tab","recheck_availability")
 	pointers.ConfigDriver.__set_button_focus(self,get_node("LineEdit"))
-#	$LineEdit.caret_position = caret_pos
 	
 
 func _visibility_changed():
 	refocus()
 	if get_position_in_parent() == 0:
 		$Label/LABELBUTTON.grab_focus()
-#	$LineEdit.caret_position = caret_pos
 var caret_pos = 0
 
 func _on_LineEdit_text_entered(new_text):
 	caret_pos = $LineEdit.caret_position
 	$LineEdit.text = new_text
+	if volatile:
+		var old_val = pointers.ConfigDriver.__get_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY)
+		if old_val != new_text:
+			triggerVolatile()
 	pointers.ConfigDriver.__store_value(CONFIG_MOD,CONFIG_SECTION,CONFIG_ENTRY,new_text)
 	get_tree().call_group("hevlib_settings_tab","recheck_availability")
-#	$Timer.start()
 
 func _process(delta):
 	caret_pos = $LineEdit.text.length()
@@ -117,3 +119,10 @@ func _process(delta):
 
 func _timeout():
 	$LineEdit.grab_focus()
+
+var updateCacheDir = "user://cache/.Mod_Menu_2_Cache/updates/has_updated.txt"
+func triggerVolatile():
+	var file = File.new()
+	file.open(updateCacheDir,File.WRITE)
+	file.store_string("1")
+	file.close()
