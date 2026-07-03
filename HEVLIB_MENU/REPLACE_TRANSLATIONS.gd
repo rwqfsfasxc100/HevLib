5e634af45143c42c20ac8b8e70e73f02aef87ad7
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-03 11:32:44 -0700

    Added licensing

--- HEVLIB_MENU/REPLACE_TRANSLATIONS.gd
+++ HEVLIB_MENU/REPLACE_TRANSLATIONS.gd
@@ -5,6 +5,35 @@ extends Node
 # Please use Translation Tracker to modify these yourself, and contact the mod author to implement them
 # https://github.com/rwqfsfasxc100/TranslationTracker/releases/latest
 
+# [license]
+# 3-Clause BSD NON-AI License
+# 
+# Copyright 2026 __hev (Benjamin Buckhurst)
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+# 
+# 4. The source code and the binary form, and any modifications made to them may not be used for the purpose of input data, the training of, or improvment of machine learning algorithms,
+# including but not limited to artificial intelligence, natural language processing, or data mining. This condition applies to any derivatives,
+# modifications, or updates based on the Software code. Any usage of the source code or the binary form in an AI-training dataset is considered a breach of this License.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# [/license]
+
 const TRANSLATIONS = {
 	"master_locale": "en",
 	"de": {
@@ -2273,6 +2302,14 @@ const TRANSLATIONS = {
 		"MODMENU2_CHANGELOG_SPACING_SIZE_TOOLTIP": {
 			"string": "Spaces placed as each indent for a changelog's subsection",
 			"version_hash": 1698732061
+		},
+		"HEVLIB_CONFIG_POINTER_LOGGING_FRAME_INTERVAL": {
+			"string": "Pointer logging interval (phys frames)",
+			"version_hash": 346203813
+		},
+		"HEVLIB_CONFIG_POINTER_LOGGING_FRAME_INTERVAL_TOOLTIP": {
+			"string": "The number of physics frames between automatic writes to the pointer log (i.e. information logged specifically by mods to the cache)\n\nThis is dependant on your current physics framerate.",
+			"version_hash": 2000819806
 		}
 	},
 	"uk_UA": {
