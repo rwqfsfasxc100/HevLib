59f716244d116402277efd3eec17d0117d399e7e
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-05 00:39:17 -0700

    Currently playing label now no longer displays above the mod menu

--- scenes/better_title_screen/CurrentlyPlaying.gd
+++ scenes/better_title_screen/CurrentlyPlaying.gd
@@ -1,11 +1,19 @@
 extends "res://menu/CurrentlyPlaying.gd"
 
+func _ready():
+	yield (get_tree(), "idle_frame")
+	var mod_menu = get_tree().get_root().find_node("ModMenu", true, false)
+	if mod_menu:
+		mod_menu.connect("visibility_changed", self , "display")
+
+var pointers = ModLoader._savedObjects[0]
 func display():
-	var pointers = CurrentGame.get_tree().get_root().get_node_or_null("HevLib~Pointers")
-	if pointers == null:
-		yield(get_tree(),"idle_frame")
-		pointers = get_tree().get_root().get_node_or_null("HevLib~Pointers")
-	if pointers.ConfigDriver.__get_value("HevLib","HEVLIB_CONFIG_SECTION_DRIVERS","show_currently_playing"):
+	var mod_menu = get_tree().get_root().find_node("ModMenu", true, false)
+	if mod_menu and mod_menu.visible:
+		visible = false
+		return
+		
+	if pointers.ConfigDriver.__get_value("HevLib", "HEVLIB_CONFIG_SECTION_DRIVERS", "show_currently_playing"):
 		.display()
 	else:
 		visible = false
