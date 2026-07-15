e8d89f297bc51f995826ca2befc9aad9e8030839
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-15 15:41:33 -0700

    Fixed procedural overlay not correctly being offset for specific mineral bases

--- scenes/minerals/mineral.gd
+++ scenes/minerals/mineral.gd
@@ -5,19 +5,9 @@ export var ferrous = false
 
 func _ready():
 	var o : Sprite = load("res://HevLib/scenes/minerals/icons/MineralOverlay.tscn").instance()
-	var stream1 = StreamTexture.new()
-	stream1.load_path = "res://HevLib/scenes/minerals/icons/minerals-c-overlay.stex"
-	o.texture = stream1
-	var stream2 = StreamTexture.new()
-	stream2.load_path = "res://HevLib/scenes/minerals/icons/minerals-n-overlay.stex"
-	o.normal_map = stream2
-#	var stream3 = StreamTexture.new()
-#	stream3.load_path = "res://HevLib/scenes/minerals/icons/minerals-m-overlay.stex"
-#	o.material.set_shader_param("map",stream3)
 	o.region_rect = sprite.region_rect
 	o.scale = sprite.scale
-	o.region_enabled = true
-	o.name = "Overlay"
+	o.position = sprite.position
 	add_child(o)
 	sprite.modulate = color
 	name = mineral
