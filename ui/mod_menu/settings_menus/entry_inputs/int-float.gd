8b8ebdde244b5021ac51ea1b9bda62b9aff5cbde
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-30 21:58:00 -0700

    Updated to 1.15.26

--- ui/mod_menu/settings_menus/entry_inputs/int-float.gd
+++ ui/mod_menu/settings_menus/entry_inputs/int-float.gd
@@ -232,13 +232,14 @@ func _input(event):
 		if Input.is_action_just_pressed("ui_accept"):
 			_timeout()
 			get_viewport().set_input_as_handled()
-		elif Input.is_action_just_pressed("ui_cancel"):
-			box.focus_mode = Control.FOCUS_NONE
-			focus_button.grab_focus()
-			get_viewport().set_input_as_handled()
+#		elif Input.is_action_just_pressed("ui_cancel"):
+#
+#			focus_button.grab_focus()
+#			get_viewport().set_input_as_handled()
 	elif boxfocus:
 		var action_passed = false
 		if Input.is_action_just_pressed("ui_cancel") or Input.is_action_just_pressed("ui_accept"):
+			box.focus_mode = Control.FOCUS_NONE
 			focus_button.grab_focus()
 			get_viewport().set_input_as_handled()
 		else:
