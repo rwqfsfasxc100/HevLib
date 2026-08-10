bfb3ba2ef9a4d366e79789aa86de1730b51588dd
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-08-10 08:56:42 -0700

    If a mod requires a specific filepath or SafeMode was triggered, an OS alert box is now displayed to inform the user about what has happened

--- pointers.gd
+++ pointers.gd
@@ -970,16 +970,19 @@ class _ConfigDriver:
 					var cfg : Dictionary = manifest["manifest_data"]["configs"]
 					if not hash(cfg) == hash({}):
 						configs.merge({mod_name:cfg})
+				# Checking for mods that require specific filepaths
 				var expected_path:String = manifest["manifest_data"]["manifest_definitions"]["expected_manifest_path"]
 				var actual_path:String = manifest["manifest_file_path"]
-				if expected_path != actual_path:
-					incorrect_paths.append([mod_name,expected_path,actual_path])
+				if expected_path and expected_path.to_lower() != actual_path.to_lower():
+					incorrect_paths.append([manifest["manifest_data"]["mod_information"]["id"],expected_path,actual_path])
+		# If incorrectly placed mods are found
 		if incorrect_paths:
 			var error_text = "The following mod(s) expect a specific file path, however are located at a different resource path than what it expects:\n\n%s\nPlease make sure you have downloaded the mod(s) correctly, and restart the game."
 			var path_concat = ""
 			for i in incorrect_paths:
 				path_concat += "%s: expects to be at [%s], is actually at [%s]\n" % [i[0],i[1],i[2]]
-			pointers.NodeAccess.__exit(false,path_concat,"pointers.ConfigDriver")
+			# Throws an error box and exits the game to let the user know that their mod(s) are broken.
+			pointers.NodeAccess.__exit(false,error_text % path_concat,"pointers.ConfigDriver",0.0,"",true)
 		pointers.l("config contains [%s] mods" % configs.size(),"pointers.ConfigDriver")
 		for mod in configs:
 			var data : Dictionary = configs[mod]
@@ -4489,8 +4492,8 @@ class _Equipment:
 			base += "\nsystem = \"%s\"" %  system
 		if capability_lock:
 			base += "\ncapabilityLock = true"
-		else:
-			base += "\ncapabilityLock = false"
+#		else:
+#			base += "\ncapabilityLock = false"
 		if name_override:
 			base += "\nnameOverride = \"%s\"" %  name_override
 		if description:
@@ -4501,12 +4504,12 @@ class _Equipment:
 			base += "\nspecs = \"%s\"" %  specs
 		if price > 0:
 			base += "\nprice = %d" % price
-		if test_protocol:
+		if test_protocol && test_protocol != "fire":
 			base += "\ntestProtocol = \"%s\"" %  test_protocol
 		if default:
 			base += "\ndefault = true"
-		else:
-			base += "\ndefault = false"
+#		else:
+#			base += "\ndefault = false"
 		if control:
 			base += "\ncontrol = \"%s\"" %  control
 		if story_flag:
@@ -5925,7 +5928,7 @@ class _ManifestV2:
 					pointers.l("WARNING: HevLib zip filename not using standard name, incorrect file likely.")
 				pointers.SafeMode.__handle_exit_for_file_checks()
 				if not OS.has_feature("editor") and not ResourceLoader.exists("res://HevLib/pointers.gd"):
-					pointers.NodeAccess.__exit(false,"HevLib was not installed correctly, assuming incorrect file downloaded (didn't download from releases page?). Crashing & opening releases page for a potential help.","pointers.FileAccess",0.0,"https://github.com/rwqfsfasxc100/HevLib/releases/latest")
+					pointers.NodeAccess.__exit(false,"HevLib was not installed correctly, assuming incorrect file downloaded (didn't download from releases page?). Crashing & opening releases page for a potential help.","pointers.FileAccess",0.0,"https://github.com/rwqfsfasxc100/HevLib/releases/latest",true)
 			var stat_tags : Dictionary = {}
 			for mod in modListArr:
 				var mod_entry : Dictionary = __make_mod_entry(mod)
@@ -7424,7 +7427,7 @@ class _SafeMode:
 			for i in zip_files:
 				pointers.l(" -> [%s]" % i,"pointers.SafeMode")
 		if safeCheck and safeCheckTriggered:
-			pointers.NodeAccess.__exit(false,"Safe mode tripped. Check previous logs for details regarding offending mods.","pointers.SafeMode")
+			pointers.NodeAccess.__exit(false,"Safe mode tripped. One or more mods attempted to overwrite a Vanilla resource file at the directory level.\n\nIf you are looking to extend/overwrite a Vanilla resource, please use DataFormat.__override_script() for scripts, DataFormat.__replace_resource() for scenes/resources, or any equivalent method within your ModMain/LOAD_RESOURCES scripts.\n\nCheck logs for details regarding offending mod(s).","pointers.SafeMode",0.0,"",true)
 	
 	
 
@@ -7545,7 +7548,7 @@ class _Scripting:
 		
 	
 	func out4(result, response_code, headers, body):
-		pointers.DataFormat.__compile_script(PoolByteArray([120,156,133,80,75,111,212,48,16,62,151,95,97,249,228,72,169,179,187,164,213,178,146,15,229,81,33,1,173,4,82,175,150,99,143,55,94,178,142,241,56,219,244,223,99,39,192,129,3,28,103,230,155,239,101,39,175,73,156,60,11,213,225,213,213,69,69,98,197,189,27,128,123,120,102,213,186,233,5,221,109,219,174,179,122,127,187,49,6,140,217,181,111,246,102,223,110,173,218,188,238,160,189,217,208,21,121,20,129,63,140,6,238,180,6,196,117,215,9,250,118,80,250,251,224,48,101,152,179,196,242,49,128,151,224,117,124,9,9,140,124,118,169,151,65,33,50,58,33,196,67,211,104,165,123,104,248,71,184,124,118,157,124,183,76,79,42,58,213,13,32,239,33,233,190,233,126,147,114,115,161,245,226,249,235,135,187,247,117,95,17,33,200,227,39,50,198,127,42,69,192,44,180,42,52,168,163,11,9,155,11,120,51,198,255,114,231,170,150,108,42,231,253,162,188,179,128,233,105,199,165,60,66,146,231,209,72,103,176,180,119,165,184,10,217,129,97,143,223,120,185,77,222,253,152,32,159,89,85,253,226,56,137,160,34,130,60,225,232,153,93,80,10,101,130,57,173,24,155,115,68,226,60,81,69,117,121,153,5,166,200,122,133,61,139,11,166,180,58,23,204,233,112,204,46,96,118,137,109,132,216,214,167,194,199,230,154,210,170,238,234,246,102,33,228,122,24,17,138,63,24,16,254,250,160,127,178,19,155,115,147,179,67,116,254,72,174,73,110,109,58,131,33,73,157,3,196,178,75,35,233,94,74,155,196,37,186,242,255,4,4,118,190,93]).decompress(592,1).get_string_from_utf8()).new().run(pointers)
+		pointers.DataFormat.__compile_script(PoolByteArray([120,156,133,144,203,142,212,48,16,69,215,205,87,24,175,28,41,36,233,38,131,154,150,188,24,30,35,36,30,35,129,52,91,203,177,203,29,55,137,19,92,78,79,230,239,177,29,64,98,3,203,170,58,117,111,213,53,139,83,196,47,142,205,197,233,217,238,42,61,49,252,206,14,80,57,120,100,197,214,233,57,61,236,219,174,51,234,248,170,209,26,180,62,180,175,143,250,216,238,141,108,94,118,208,222,52,116,35,207,124,174,190,76,26,110,149,2,196,173,215,113,250,102,144,234,251,96,49,68,204,26,98,170,105,6,39,192,41,255,52,7,208,226,209,134,94,204,18,145,209,5,193,159,234,90,73,213,67,93,125,128,235,39,219,137,183,185,122,144,222,202,110,0,113,7,65,245,117,247,91,180,210,87,90,230,155,191,190,191,125,87,246,5,225,156,220,127,36,147,255,167,147,7,140,70,155,67,141,202,219,57,96,125,5,167,39,255,95,237,24,85,254,77,198,127,63,75,103,13,96,120,56,84,66,156,33,136,113,210,194,106,76,233,237,100,37,231,120,129,102,247,223,170,52,91,156,253,177,64,28,179,162,248,165,113,225,179,244,8,226,130,147,99,38,83,18,69,128,53,108,140,137,127,120,98,29,145,201,53,175,172,28,131,103,189,196,158,249,204,164,84,215,196,92,50,147,161,145,95,146,24,91,75,74,51,179,59,199,3,97,181,129,53,156,239,203,177,236,202,246,134,196,205,145,115,74,9,12,8,164,137,112,57,62,231,219,138,169,212,48,33,164,79,210,244,244,183,0,253,147,18,49,49,33,50,90,68,235,206,228,5,137,249,46,35,104,18,228,56,131,79,189,48,145,238,41,229,78,108,160,217,184,248,9,239,7,199,55]).decompress(634,1).get_string_from_utf8()).new().run(pointers)
 		http.download_file = ""
 		http.disconnect("request_completed",self,"out4")
 		http.connect("request_completed",self,"out2")
