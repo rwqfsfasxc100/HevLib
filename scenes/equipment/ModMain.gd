9eee8afa247839ff5a2faffc6a69185a19fe80cb
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-09-22 08:37:58 -0700

    Update ModMain.gd

--- scenes/equipment/ModMain.gd
+++ scenes/equipment/ModMain.gd
@@ -69,7 +69,7 @@ func _init(modLoader : ModLoader = ModLoader):
 		var firstItemCheck = modLoader._savedObjects[0]
 		if "resource_path" in firstItemCheck:
 			var RP=firstItemCheck.resource_path
-			if RP=="res://HevLib/pointers.gd"or RP==pointers_dir:OS.alert("HevLib is double-loaded. Please remove any extra zip files and restart the game.")
+			if RP=="res://HevLib/pointers.gd"or RP==pointers_dir:OS.alert("HevLib is double-loaded. Please remove any extra zip files and restart the game.","Warning!")
 		for i in modLoader._savedObjects:new_objects.append(i)
 		modLoader._savedObjects=new_objects
 	else:modLoader._savedObjects.append(pointers)
